996ea56da269606edc496a1a5adae7c7889904e2
Lily Jordan <None>
2023-07-27 17:21:50 -0700

    initial commit

---
+++ src/DAC.sol
@@ -0,0 +1,107 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+using SafeMath for uint256;
+
+contract DAC {
+    address payable public sponsor;
+    address public arbitrator;
+    uint256 public deadline;
+    uint256 public goal;
+    uint256 public contrib_comp_pct;
+    uint256 public sponsor_comp_pct;
+
+    mapping(address => uint256) public contributions;
+    uint256 public totalContributions;
+
+    enum State { Funding, Funded, Approved, Failed }
+    State public state;
+
+    constructor(
+        address payable _sponsor,
+        address _arbitrator,
+        uint256 _deadline,
+        uint256 _goal,
+        uint256 _contrib_comp_pct,
+        uint256 _sponsor_comp_pct
+    ) {
+        sponsor = _sponsor;
+        arbitrator = _arbitrator;
+        deadline = _deadline;
+        goal = _goal;
+        contrib_comp_pct = _contrib_comp_pct;
+        sponsor_comp_pct = _sponsor_comp_pct;
+        state = State.Funding;
+    }
+
+    function contribute() public payable {
+        require(state == State.Funding, "Not in funding state");
+        require(block.timestamp < deadline, "Deadline passed");
+
+        uint256 potentialTotal = totalContributions + msg.value;
+        uint256 goalPlusSponsorComp =  goal.mul(100.add(sponsor_comp_pct)).div(100);
+
+        if (potentialTotal > goalPlusSponsorComp) {
+            revert("Contribution would exceed goal");
+        } else if (potentialTotal == goalPlusSponsorComp) {
+            state = State.Funded;
+        }
+
+        contributions[msg.sender] += msg.value;
+        totalContributions += msg.value;
+    }
+
+    function approvePayout(address payable founder) public {
+        require(msg.sender == arbitrator, "Only arbitrator can authorize payout");
+        require(state == State.Funded, "Not funded");
+        state = State.Approved;
+        founder.transfer(address(this).balance);  // Whole function reverts if this fails
+        // TODO pay back the potential interest the sponsor put in
+    }
+
+    function rejectPayout() public {
+        require(msg.sender == arbitrator, "Only arbitrator can authorize payout");
+        require(state == State.Funded, "Not funded");
+        require(block.timestamp >= deadline, "Deadline not passed");
+        state = State.Failed;
+    }
+
+    function checkFailure() public {
+        require(state == State.Funding, "Not in funding state");
+        require(block.timestamp >= deadline, "Deadline not passed");
+        state = State.Failed;
+    }
+
+    function refund() public {
+        require(state == State.Failed, "Not in failure state");
+        uint256 amount = contributions[msg.sender] * (1 + contrib_comp_pct);
+        require(amount <= address(this).balance, "Not enough balance");
+        contributions[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    // TODO after the deadline, let sponsor claim all unowed interest (eg if the goal is 60% achieved, give the sponsor back 40% of the interest)
+}
+
+contract DACFactory {
+    DAC[] public contracts;
+
+    function createDAC(
+        address _arbitrator,
+        uint256 _deadline,
+        uint256 _goal,
+        uint256 _contrib_comp_pct,
+        uint256 _sponsor_comp_pct
+    ) public payable returns (DAC) {
+        require(msg.value >= _goal.mul(100.add(_contrib_comp_pct)).div(100), "Insufficient sponsor fund");
+        require(msg.value <= _goal.mul(100.add(_contrib_comp_pct)).div(100), "Overfunded");  // TODO say how much is needed (for both of these)
+        DAC dac = new DAC(payable(msg.sender), _arbitrator, _deadline, _goal, _contrib_comp_pct, _sponsor_comp_pct);
+        contracts.push(dac);
+        return dac;
+    }
+
+    function getContracts() public view returns (DAC[] memory) {
+        return contracts;
+    }
+}
---
+++ test/DAC.t.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {DAC, DACFactory} from "../src/DAC.sol";
+
+contract DACTest is Test {
+    DAC public dac;
+    address payable public contributor1;
+    address payable public contributor2;
+    address payable public contributor3;
+    address payable public sponsor;
+    address payable public founder;
+    address public arbitrator;
+    uint256 public deadline;
+    uint256 public goal;
+    uint256 public contrib_comp_pct;
+    uint256 public sponsor_comp_pct;
+
+    function setUp() public {
+        sponsor = payable(vm.addr(1));
+        vm.deal(sponsor, 1050);
+
+        address payable[3] memory contributors = [contributor1, contributor2, contributor3];
+            for (uint i=0; i<contributors.length; i++) {
+            vm.deal(contributors[i], 10000);
+        }
+
+        founder = payable(vm.addr(2));
+        arbitrator = vm.addr(3);
+        deadline = 1687669795;  // Unix timestamp, 6/25/2023
+        goal = 1000;
+        contrib_comp_pct = 5;
+        sponsor_comp_pct = 10;
+
+        console.log(sponsor_comp_pct);
+        console.log(sponsor_comp_pct / 100);
+        console.log('total goal:', goal * (1 + (sponsor_comp_pct / 100)));
+
+        DACFactory factory = new DACFactory();
+        vm.prank(sponsor);
+        dac = factory.createDAC{ value: goal * contrib_comp_pct / 100 }(arbitrator, deadline, goal, contrib_comp_pct, sponsor_comp_pct);
+    }
+
+    function testContribute() public {
+      vm.startPrank(contributor1);
+      uint256 amount1 = 300;
+      dac.contribute{ value: amount1 }();
+      vm.stopPrank();
+
+      vm.startPrank(contributor2);
+      uint256 amount2 = 400;
+      dac.contribute{ value: amount2 }();
+      vm.stopPrank();
+
+      vm.startPrank(contributor3);
+      uint256 amount3 = 500;
+      dac.contribute{ value: amount3 }();
+      vm.stopPrank();
+      // TODO check that the contract gets the money and the contributors lose it
+    }
+
+    function testApprovePayout() public {
+        console.log('before:', contributor1.balance);
+        vm.startPrank(contributor1);
+        dac.contribute{ value: 1100 }();
+        vm.stopPrank;
+
+        vm.prank(arbitrator);
+        dac.approvePayout(founder);
+        console.log(sponsor.balance);
+        console.log(founder.balance);
+        console.log(contributor1.balance);
+        console.log(contributor2.balance);
+        // TODO check that sponsor has same balance
+        // TODO check that contributors have same balance
+        // TODO check that founder gets the transfer
+    }
+}
